2ee957e6c008c78882f5a8aed20c3ca09d11e101
Mohamed Sayed <mohameddsayedd17@gmail.com>
2026-07-25 03:31:18 +0300

    Upgrade CHECK1

--- src/test/java/factory/BrowserOptionsFactory.java
+++ src/test/java/factory/BrowserOptionsFactory.java
@@ -41,27 +41,31 @@ public class BrowserOptionsFactory {
     }
 
     public static EdgeOptions getEdgeOptions() {
+
         EdgeOptions options = new EdgeOptions();
-       // options.addArguments("--headless=new");
+
+        options.addArguments("--headless=new");
         options.addArguments("--window-size=1920,1080");
+
         options.addArguments("--disable-gpu");
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
+
         options.addArguments("--disable-extensions");
         options.addArguments("--disable-popup-blocking");
+
         options.addArguments("--remote-allow-origins=*");
+
         options.addArguments("--ignore-certificate-errors");
-     //   options.addArguments("--proxy-server=direct://");
-        //options.addArguments("--proxy-bypass-list=*");
-        // Prevents Edge's first-run experience, which blocks headless startup
-        // under service accounts like SYSTEM and causes the DevToolsActivePort crash
+
         options.addArguments("--no-first-run");
         options.addArguments("--no-default-browser-check");
         options.addArguments("--disable-background-networking");
-        // Unique profile dir per run avoids profile-lock conflicts when
-        // Chrome/Edge/Firefox run in parallel (testNG.xml uses parallel="tests")
-    //    options.addArguments("--user-data-dir=" + System.getProperty("java.io.tmpdir")
-       //         + "edge-profile-" + System.nanoTime());
+
+        options.addArguments("--user-data-dir="
+                + System.getProperty("java.io.tmpdir")
+                + "/edge-profile-"
+                + System.nanoTime());
 
         return options;
     }
